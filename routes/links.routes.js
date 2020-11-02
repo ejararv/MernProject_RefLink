725d5022edf5821258c1d4e3e28322041530a0f1
ejararv <ejararv@gmail.com>
2020-11-02 19:01:17 +0100

    Added 4 Pages to client

---
+++ routes/links.routes.js
@@ -0,0 +1,38 @@
+const {Router} = require('express')
+const router = Router()
+const Link = require('../models/Link')
+
+router.post('/generate', async (req, res) => {
+    try{
+
+    } catch (e) {
+        res.status(500).json({message: 'Wystapil blad'})    
+    }
+} 
+)
+
+router.get( '/', async (req, res) =>  {
+    try{
+        const links = await Link.find({ owner: null }) // 
+        res.json(links )
+
+    } catch (e) {
+        res.status(500).json({message: 'Wystapil blad'})    
+    }
+}
+)
+
+router.get( '/:id', async (req, res) =>  {
+    try{
+
+        const link = await Link.findById(req.params.id)
+        res.json(Link)
+    } catch (e) {
+        res.status(500).json({message: 'Wystapil blad'})    
+    }
+}
+)
+
+
+
+module.exports = router
