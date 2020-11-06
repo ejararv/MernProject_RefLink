d107cd41c259e58172f407712af89d9cc94746d6
ejararv <ejararv@gmail.com>
2020-11-06 18:05:35 +0100

    Fix problems with http.hook function

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,13 @@
 import React, {useContext, useEffect, useState} from 'react'
+import { useHttp } from '../hooks/http.hook'
 
 
 
 export const AuthPage = () => {
-  const [form ,serForm] = useState ({    //useState Передаёт данные в виде массива где 1 значение это данные из формы а 2значение это функция для формы
+
+  const {loading, error, request} = useHttp()
+
+  const [form ,setForm] = useState ({    //useState Передаёт данные в виде массива где 1 значение это данные из формы а 2значение это функция для формы
     email: '' , password:''
   })
 
@@ -12,12 +16,30 @@ export const AuthPage = () => {
    const changeHandler = event => {
      setForm ({ ...form , [event.target.name]: event.target.value })  
    }
+
+
+
+   const  registerHandler  = async () =>
+   {
+     try{
+      const data = await request('/api/auth/register', 'POST', {...form})
+      console.log('Data', data)
+     }catch(e) {
+
+     }
+   }
+  
   
 
   return (
     <div className="row">
       <div className="col s6 offset-s3">
         <h1>Referal Link</h1>
+
+
+        <div>
+    <h1> TEST</h1>
+  </div>
         <div className="card blue darken-1">
           <div className="card-content white-text">
             <span className="card-title">Zaloguj sie</span>
@@ -56,13 +78,15 @@ export const AuthPage = () => {
             <button
               className="btn yellow darken-4"
               style={{marginRight: 10}}
+              disabled={loading} 
              
             >
               Zaloguj
             </button>
             <button
               className="btn grey lighten-1 black-text"
-             
+             onClick={registerHandler}
+             disabled={loading}
             >
              Rejestracja
             </button>
@@ -71,4 +95,11 @@ export const AuthPage = () => {
       </div>
     </div>
   )
+
+return (
+  <div>
+    <h1> TEST</h1>
+  </div>
+)
+
 }
