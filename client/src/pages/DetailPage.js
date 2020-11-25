9e2920485a7fab556c6b9e94d25c6f571fb131a9
Lizaveta <sobaleva.lizaveta@gmail.com>
2020-11-25 22:17:05 +0100

    Fix problems with authorization +addet routes

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export const UserPage = () => {
+export const DetailPage = () => {
     return (
         <div>
-            <h1> UserPage </h1>
+            <h1> DetailPage </h1>
         </div>
     )
 }
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,40 +1,34 @@
 import React from 'react'
-import { Route, Switch, Redirect } from 'react-router-dom'
-import { AuthPage } from './pages/AuthPage'
-import { LinksPage } from './pages/LinksPage'
-import { CreatePage } from './pages/CreatePage'
-import { UserPage } from './pages/UserPage'
-import { RegisrationPage } from './pages/RegistrationPage'
+import {Switch, Route, Redirect} from 'react-router-dom'
+import {LinksPage} from './pages/LinksPage'
+import {CreatePage} from './pages/CreatePage'
+import {DetailPage} from './pages/DetailPage'
+import {AuthPage} from './pages/AuthPage'
 
 export const useRoutes = isAuthenticated => {
-
-    if (isAuthenticated) {
-        return (
-            <Switch>
-                <Route path="/links" exact>
-                    <LinksPage />
-                </Route>
-
-                <Route path="/user/:id" >
-                    <UserPage />
-                </Route>
-                <Redirect to="/create" />
-                <CreatePage/>
-            </Switch>
-        )
-    }
+  if (isAuthenticated) {
     return (
-        <Switch>
-            <Route path="/" exact>
-                <AuthPage />
-            </Route>
-            
-            
-
-            <Route path="/registration" exact>
-                    <RegisrationPage />
-                </Route>
-            <Redirect to="/registration" />
-        </Switch>
+      <Switch>
+        <Route path="/links" exact>
+          <LinksPage />
+        </Route>
+        <Route path="/create" exact>
+          <CreatePage />
+        </Route>
+        <Route path="/detail/:id">
+          <DetailPage />
+        </Route>
+        <Redirect to="/create" />
+      </Switch>
     )
+  }
+
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <AuthPage />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  )
 }
