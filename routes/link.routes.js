933927961f00d12b24ee0551b187b1e84825377b
Lizaveta <sobaleva.lizaveta@gmail.com>
2020-12-06 18:00:46 +0100

    Fix problems whith add link

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -1,64 +1,53 @@
 const {Router} = require('express')
-const router = Router()
+const config = require('config')
+const shortid = require('shortid')
 const Link = require('../models/Link')
+const auth = require('../middleware/auth.middleware')
+const router = Router()
 
-router.post('/generate', async (req, res) => {
-    try{
+router.post('/generate', auth, async (req, res) => {
+  try {
+    const baseUrl = config.get('baseUrl')
+    const {from} = req.body
 
-    } catch (e) {
-        res.status(500).json({message: 'Wystapil blad'})    
+    const code = shortid.generate()
+
+    const existing = await Link.findOne({ from })
+
+    if (existing) {
+      return res.json({ link: existing })
     }
-} 
-)
 
-router.get( '/', async (req, res) =>  {
-    try{
-        const links = await Link.find({ owner: null }) // 
-        res.json(links )
+    const to = baseUrl + '/t/' + code
 
-    } catch (e) {
-        res.status(500).json({message: 'Wystapil blad'})    
-    }
-}
-)
+    const link = new Link({
+      code, to, from, owner: req.user.userId
+    })
 
-router.get( '/:id', async (req, res) =>  {
-    try{
+    await link.save()
 
-        const link = await Link.findById(req.params.id)
-        res.json(Link)
-    } catch (e) {
-        res.status(500).json({message: 'Wystapil blad'})    
-    }
-}
-)
-try{
+    res.status(201).json({ link })
+  } catch (e) {
+    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+  }
+})
 
-} catch (e) {
-res.status(500).json({message: 'Wystapil blad'})
-}
+router.get('/', auth, async (req, res) => {
+  try {
+    const links = await Link.find({ owner: req.user.userId })
+    res.json(links)
+  } catch (e) {
+    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+  }
+})
 
-
-
-router.get( '/', async (req, res) => {
-try{
-
-} catch (e) {
-res.status(500).json({message: 'Wystapil blad'})
-}
-}
-)
-
-router.get( '/:id', async (req, res) => {
-try{
-
-} catch (e) {
-res.status(500).json({message: 'Wystapil blad'})
-}
-
-}
-)
-
-
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const link = await Link.findById(req.params.id)
+    res.json(link)
+  } catch (e) {
+    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+  }
+})
 
 module.exports = router
